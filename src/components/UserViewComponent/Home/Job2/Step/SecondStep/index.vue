0485ab40086e55a3fa5b215d97f9f6fb486133ff
toilahtc96 <toilahtc@gmail.com>
2023-08-21 22:30:40 +0700

    update step

--- src/components/UserViewComponent/Home/Job2/Step/SecondStep/index.vue
+++ src/components/UserViewComponent/Home/Job2/Step/SecondStep/index.vue
@@ -5,13 +5,39 @@ import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { JobService } from "@/services/jobService";
 import { message } from "ant-design-vue";
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-// const editor = ClassicEditor;
-// const editorData = ref('<p>Content of the editor.</p>');
-// const editorConfig = ref({
-//     // The configuration of the editor.
-// });
+
+import { h } from 'vue';
+import {
+    UserOutlined,
+    SolutionOutlined,
+    LoadingOutlined,
+    SmileOutlined,
+} from '@ant-design/icons-vue';
+import { StepProps } from 'ant-design-vue';
+const items = [
+    {
+        title: 'Login',
+        status: 'finish',
+        icon: h(UserOutlined),
+    },
+    {
+        title: 'Verification',
+        status: 'finish',
+        icon: h(SolutionOutlined),
+    },
+    {
+        title: 'Pay',
+        status: 'process',
+        icon: h(LoadingOutlined),
+    },
+    {
+        title: 'Done',
+        status: 'wait',
+        icon: h(SmileOutlined),
+    },
+] as StepProps[];
+
 const route = useRoute();
 
 const id = +route.params.id;
@@ -25,14 +51,14 @@ const getAgency = (agencyId: number) => {
     })
 }
 onMounted(() => {
-    if (props.agencyId) {
-        getAgency(props.agencyId);
-    }
-    if (!props.agencyId) {
-        jobService.getJobById(id)?.then((data) => {
-            getAgency(data.agencyId);
-        })
-    }
+    // if (props.agencyId) {
+    //     getAgency(props.agencyId);
+    // }
+    // if (!props.agencyId) {
+    //     jobService.getJobById(id)?.then((data) => {
+    //         getAgency(data.agencyId);
+    //     })
+    // }
 })
 const formState = ref({
     "description": ""
@@ -40,39 +66,10 @@ const formState = ref({
 const onFinish = () => {
     message.success('success');
 }
+const editStep = () => {
+
+}
 </script>
-
 <template>
-    <div class="container">
-        <div class="col-xxl-12">
-            <p>Đối tác của bạn đang tiến hành gửi CV của bạn tới công ty chủ quản, Hoạt động này thường xảy ra
-                trong khoảng 1 đến 2 ngày. </p>
-            <div style="float: left;">
-                Thông tin ứng tuyển
-            </div>
-        </div>
-
-
-        <div class="col-xxl-12">
-            <p>Đối tác của bạn đang tiến hành gửi CV của bạn tới công ty chủ quản, Hoạt động này thường xảy ra
-                trong khoảng 1 đến 2 ngày. </p>
-            <div style="float: left;">
-                Thông tin ứng tuyển
-            </div>
-        </div>
-        <div class="col-xxl-12">
-            <p>Đối tác của bạn đang tiến hành gửi CV của bạn tới công ty chủ quản, Hoạt động này thường xảy ra
-                trong khoảng 1 đến 2 ngày. </p>
-            <div style="float: left;">
-                Thông tin ứng tuyển
-            </div>
-        </div>
-        <div class="col-xxl-12">
-            <p>Đối tác của bạn đang tiến hành gửi CV của bạn tới công ty chủ quản, Hoạt động này thường xảy ra
-                trong khoảng 1 đến 2 ngày. </p>
-            <div style="float: left;">
-                Thông tin ứng tuyển
-            </div>
-        </div>
-    </div>
+    <a-steps :items="items"></a-steps>
 </template>
