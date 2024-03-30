64f7f9b2ca7309785e1cd43e9edddc17f10b2a03
toilahtc96 <toilahtc@gmail.com>
2024-03-30 16:33:44 +0700

    update https

--- src/core/httpClient.ts
+++ src/core/httpClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://https://d1wiizqgthxah8.cloudfront.net/';
+axios.defaults.baseURL = 'https://d1wiizqgthxah8.cloudfront.net/';
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
